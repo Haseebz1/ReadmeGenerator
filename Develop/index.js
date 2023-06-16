2cd6646c9f803ea4b9a0c5d62b6687ff69c25f32
Haseebz1 <haseebkandore@hotmail.com>
2023-06-16 01:45:15 +0100

    array of questions added

--- Develop/index.js
+++ Develop/index.js
@@ -1,8 +1,57 @@
 // TODO: Include packages needed for this application
+const fs = require("fs");
+const inquirer = require("inquirer");
+const generate = require("./utils/generateMarkdown");
 
 
 // TODO: Create an array of questions for user input
-
+const questions = [
+    {
+      type: "input",
+      name: "github",
+      message: "What is your github username?",
+    },
+    {
+      type: "input",
+      name: "email",
+      message: "What is your Email?",
+    },
+    {
+      type: "input",
+      name: "title",
+      message: "What is name of project?",
+    },
+    {
+      type: "input",
+      name: "description",
+      message: "Write description of project?",
+     
+    },
+    {
+      type: "list",
+      name: "license",
+      message: "What kind of license should your project have?",
+      choices: ["NONE", "APACHE 2.0", "MIT", "BSD 3"],
+    },
+    {
+      type: "input",
+      name: "test",
+      message: "What is command for test?",
+  
+    },
+    {
+      type: "input",
+      name: "installation",
+      message: "write command for install dependencies?",
+  
+    },
+    {
+      type: "input",
+      name: "usage",
+      message: "What does the user need to know about using repo?",
+  
+    },
+  ];
 
 
 // TODO: Create a function to write README file
